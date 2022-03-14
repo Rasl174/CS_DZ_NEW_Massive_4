1dad6249fe68b2a93d59f76eac88ab9cdfcb48d2
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-14 10:47:36 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_NEW_Massive_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_NEW_Massive_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int shift;
+
+            Console.WriteLine("На сколько сдвинуть массив: ");
+            shift = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0 ; i < numbers.Length; i++)
+            {
+                numbers[i] -= shift;
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
